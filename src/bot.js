d41beb1f5c15611a379a72262a8610546b282f8a
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-11-14 09:41:24 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,27 +1,16 @@
+// src/bot.js (خلاصه)
 const { Telegraf } = require('telegraf');
-const { config } = require('./config');
-const { register: regTriggers } = require('./features/triggers');
-const { register: regGate } = require('./features/actions/gateActions');
-const { register: regMicro } = require('./features/actions/microActions');
-const { register: regCancel } = require('./features/actions/cancelActions');
-const { register: regNav } = require('./features/actions/navActions');
-const { register: regAdmin } = require('./features/commands/adminCommands');
-const { register: regMicroAdmin } = require('./features/commands/microAdmin');
-const { register: regLinkWizard } = require('./features/commands/linkWizard');
-const { register: regJoin } = require('./features/joinHandlers');
+const linkWizard = require('./features/commands/linkWizard');
+// ... بقیهٔ features
 
-async function buildBot(){
-  const bot = new Telegraf(config.botToken, { handlerTimeout: 9000 });
+function buildBot(config) {
+  const bot = new Telegraf(config.botToken);
   global.bot = bot;
 
-  bot.start((ctx)=>ctx.reply('نینجا در خدمت شماست 🥷🏻'));
+  // features...
+  linkWizard.register(bot);
 
-  const me = await bot.telegram.getMe();
-  regTriggers(bot, me);
-  regGate(bot); regMicro(bot); regCancel(bot); regNav(bot);
-  regAdmin(bot, config); regMicroAdmin(bot, config); regLinkWizard(bot, config);
-  regJoin(bot, config);
-
-  return bot;
+  return bot; // نه launch و نه polling
 }
+
 module.exports = { buildBot };
