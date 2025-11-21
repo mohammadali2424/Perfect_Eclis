caeec676397645ccf96feca93532a266043f62f0
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-11-21 01:28:35 -0800

    Delete src directory

--- src/services/moderationService.js
+++
@@ -1,4 +0,0 @@
-// src/services/moderationService.js
-const { isAllowed, getState, listAll, upsert, remove, setLocked, setFreeze } = require('../domain/repositories/registeredChatsRepo');
-const { setVip, unsetVip } = require('../domain/repositories/vipRepo');
-module.exports = { isAllowed, getState, listAll, upsert, setVip, unsetVip, setLocked, setFreeze, remove };
--- src/utils/cache.js
+++
@@ -1,7 +0,0 @@
-const NodeCache = require('node-cache');
-
-const cache = new NodeCache({ stdTTL: 180, checkperiod: 120, maxKeys: 20000 });
-const inFlightUser = new NodeCache({ stdTTL: 8, checkperiod: 15 });
-const cbMap = new NodeCache({ stdTTL: 600, checkperiod: 120, maxKeys: 50000 });
-
-module.exports = { cache, inFlightUser, cbMap };
