f6645408096eab02b0ff3c59014c61a21750a29f
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-11-21 00:59:25 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,14 +3,19 @@ const config = require('./config');
 const { register: registerTriggers } = require('./features/triggers');
 const { register: registerGateActions } = require('./features/actions/gateActions');
 const { register: registerLinkWizard } = require('./features/commands/linkWizard');
+const { register: registerHelp } = require('./features/commands/help');
+const { logUpdates } = require('./middleware/logUpdates');
 
-function buildBot(){
+function buildBot() {
   const bot = new Telegraf(config.botToken, { handlerTimeout: 9000 });
 
-  // حتماً config را پاس بده
+  // لاگِ نوع آپدیت‌ها برای تشخیص اینکه اصلاً آپدیت می‌رسد یا نه
+  bot.use(logUpdates());
+
   registerTriggers(bot, config);
   registerGateActions(bot, config);
   registerLinkWizard(bot, config);
+  registerHelp(bot, config);
 
   return bot;
 }
