7c2e1d5e914f66512392591c5fb02331485e44a1
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-11-30 20:04:33 -0800

    Update types.ts

--- src/core/types.ts
+++ src/core/types.ts
@@ -3,27 +3,27 @@ import { SessionFlavor } from "grammy";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export interface SessionData {
-  // برای UI عمومی
-  ui_last_menu_id?: number;
-
-  // برای ثبت‌نام پلیر
+  // ثبت‌نام
   reg_step?: "clan" | "name";
   reg_clan: string | null;
   reg_name: string | null;
 
-  // برای پنل ادمین جهان‌ساز
-  admin_mode?:
-    | "add_spot"
-    | "add_edge_time";
+  // برای UI اگر بعداً خواستی
+  ui_last_menu_id?: number;
+
+  // پنل ادمین جهان‌ساز
+  admin_mode?: "add_spot" | "add_edge_time";
   admin_region_id?: number;
   admin_from_spot_id?: number;
   admin_to_spot_id?: number;
+  admin_edge_twosided?: boolean; // مسیر دوطرفه یا نه
 }
 
 export interface Services {
   supabase: SupabaseClient<any, any, any>;
 }
 
-export type MyContext = Context & SessionFlavor<SessionData> & {
-  services: Services;
-};
+export type MyContext = Context &
+  SessionFlavor<SessionData> & {
+    services: Services;
+  };
