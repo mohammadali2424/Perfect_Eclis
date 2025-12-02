555a57d9be9b24397ac30ba9de315d7afc068edb
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-12-02 00:30:03 -0800

    Update bot.ts

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -5,6 +5,7 @@ import { MyContext, SessionData, Services } from "./types";
 import { registerWorldAdminCommands } from "../features/world/admin-commands";
 import { registerSecurityFeature } from "../features/security/guard";
 import { registerTravelFeature } from "../features/world/travel";
+import { registerWorldVehicleShop } from "../features/world/vehicle-shop";
 import { registerWorldAdminFeature } from "../features/world/admin-builder";
 import { registerOnboardingFeature } from "../features/world/onboarding";
 
@@ -37,4 +38,5 @@ registerSecurityFeature(bot);
 registerWorldAdminCommands(bot);
 registerOnboardingFeature(bot);
 registerWorldAdminFeature(bot);
+registerWorldVehicleShop(bot);
 registerTravelFeature(bot);
