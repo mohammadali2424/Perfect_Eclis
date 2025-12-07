8fce99f2dcb897b95842e1dbca64173ac01d4017
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-12-06 21:04:15 -0800

    Update bot.ts

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -13,7 +13,7 @@ import { registerVehicleTravelFeature } from "../features/world/travel-vehicles"
 import { registerWorldAdminFeature } from "../features/worldbuilder/admin-builder";
 import { registerWorldAdminCommands } from "../features/worldbuilder/admin-commands";
 import { registerPathBuilderFeature } from "../features/worldbuilder/path-builder";
-import { registerWorldVehicleShop } from "../features/worldbuilder/vehicle-shop";
+import { registerWorldVehicleShop } from "../features/economy/vehicle-shop";
 // 👇 اگر ui.ts درست کرده‌ای، بعداً اینو برمی‌گردونیم
 // import { registerUiFeature } from "../features/ui/ui";
 
