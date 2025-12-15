6f4965755884e816b3ed2b57dcf6f875bfd00da1
mohammadali2424 <61043414+mohammadali2424@users.noreply.github.com>
2025-12-15 00:47:33 -0800

    Update travel-vehicles.ts

--- src/features/world/travel-vehicles.ts
+++ src/features/world/travel-vehicles.ts
@@ -321,10 +321,10 @@ if (char.current_region_id && char.current_spot_id) {
     char.current_spot_id
   );
 
-  const wellRes = await ctx.services.db.hasFluxWell(
-    char.current_region_id,
-    char.current_spot_id
-  );
+ const wellRes = await ctx.services.db.hasFluxWell(
+  char.current_spot_id
+);
+
 
   if (!wellRes.ok) {
     console.error("showTransportMenu hasFluxWell error:", wellRes.error);
